e25cca6a038f12a8f2f95b232970efb68b1d8cc8
RoboUmanAdm <robocode.umsew@gmail.com>
2025-05-07 18:53:00 +0300

    new commit

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const http = require('http');
 const fs = require('fs');
 const path = require('path');
 
+
 const pathToIndex = path.join(__dirname,'static','index.html');
 const indexHtmlFile = fs.readFileSync(pathToIndex);
 
@@ -10,6 +11,19 @@ const server = http.createServer((req, res)=> {
         return res.end(indexHtmlFile);
     }
     res.statusCode = 404;
-    return res.end('Error 404');
+    return res.end('Error 404 try egan leter');
 });
+
+
+
+const {Server} = require("socket.io");
+const io = new Server(server);
+io.on('connection', (socket)=> {
+    console.log('user has connected. id - ' + socket.id);
+});
+socket.on('new_message', (message)=> {
+    io.emit('message', message);
+    console.log(message);
+});
+
 server.listen(3000);
--- static/script.js
+++ static/script.js
@@ -1,1 +1,20 @@
 alert("Wellcome to chat center!!")
+const socket = io();
+
+const form = document.getElementById('form');
+const messages = document.getElementById('messages');
+const input = document.getElementById('input');
+
+form.addEventListener('submit', function(e){
+    e.preventDefault();
+    if(input.value){
+        socket.emit('new_message', input.value);
+        input.value = '';
+    }
+socket.on('massage',function(msg){
+    let item = document.createElement('li');
+    item.textContent = msg;
+    messages.appendChild(item);
+    window.scrollTo(0, document.body.scrollHeight);
+});
+});
